067340b59afbfc21c2da583e21f1f46c92bb33c3
Katlyn Williams <n754z265@wichita.edu>
2024-09-19 13:57:37 -0500

    Attempting to add Lab 3 content

--- Lab3/TextBox.cpp
+++ Lab3/TextBox.cpp
@@ -1,1 +1,22 @@
-#include "TextBox.h"
+
+#include "TextBox.h";
+
+TextBox::TextBox(std::string value) {
+	setVal(value);
+	getVal();
+}
+
+void TextBox::setVal(std::string value) {
+	TextValue = value;
+}
+
+std::string TextBox::getVal() {
+	return TextValue;
+}
+
+int main() {
+	TextBox TextBox1;
+	TextBox1.setVal("Hello");
+	TextBox1.getVal();
+	TextBox TextBox2("World");
+}
--- Lab3/TextBox.h
+++ Lab3/TextBox.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <string>;
+
 class TextBox
 {
 private: 
+	std::string TextValue;
+
 public:
+	TextBox() = default;
+	explicit TextBox(std::string Val);
+	std::string getVal();
+	void setVal(std::string Val);
+	~TextBox() = default;
+
 };
 
